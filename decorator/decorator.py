cfd03a8216e207e21b2d57319941f740ca4ceb4b
Matheus Farias <38105118+m4th21@users.noreply.github.com>
2018-12-12 02:08:32 -0200

    Update decorator.py

--- decorator/decorator.py
+++ decorator/decorator.py
@@ -2,12 +2,11 @@ import time
 
 
 def time_this(func):
-    """The time_this decorator"""
 
     def decorated(*args, **kwargs):
         start = time.time()
         result = func(*args, **kwargs)
-        print 'Rain in', time.time() - start, 'seconds'
+        print ('Rain in', time.time() - start, 'seconds')
         return result
     return decorated
 
@@ -15,9 +14,8 @@ def time_this(func):
 # Decorator syntax
 @time_this
 def count(until):
-    """Counts to 'until', then returns the result"""
 
-    print "Counting to", until, "…"
+    print ("Counting to", until, "…")
     num = 0
     for i in xrange(to_num(until)):
         num += 1
@@ -25,10 +23,9 @@ def count(until):
 
 
 def to_num(numstr):
-    """Turns a comma-separated number string to an int"""
     return int(numstr.replace(",", ""))
 
 # Run count with various values
 for number in ("10,000", "100,000", "1,000,000"):
-    print count(number)
-    print "-" * 20
+    print (count(number))
+    print ("-" * 20)
