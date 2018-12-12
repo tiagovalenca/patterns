a7014723f9f3ae902ad78ed7908a550660669062
tiagovalenca <tlv@cesar.school>
2018-12-11 23:09:35 -0300

    Create main.py

---
+++ composite/main.py
@@ -0,0 +1,15 @@
+class animals():
+   class aves():
+       class galinha():
+           tipo = "Isso é uma galinha"
+       class pombo():
+           tipo = "Isso é um pombo"
+       class pato():
+           tipo = "Isso é um pato"
+   class mamiferos():
+       class cachorro():
+           tipo = "Isso é um cachorro"
+       class golfinho():
+           tipo = "Isso é um golfinho"
+       class onitorrinco():
+           tipo = "Isso é um golfinho"
